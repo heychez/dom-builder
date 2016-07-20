185863aa5b116925a66e7e7de1e43601b1770051
Roberto Cuadros <roberto.cclo@gmail.com>
2016-07-19 19:40:04 -0500

    dom-builder init

---
+++ dom-builder.js
@@ -0,0 +1,73 @@
+/*!
+ * dom-builder.js - DOMBuilder JS v1.0.0
+ * ---------------------------------------------------------
+ * NPM:    npmjs.com/package/dom-builder
+ * GitHub: github.com/heychez/dom-builder
+ * ---------------------------------------------------------
+ * Author: Roberto C. Cuadros Loayza
+ * Email:  roberto.cclo@gmail.com
+ * Www:    blup.space
+ * GitHub: github.com/heychez
+ * ---------------------------------------------------------
+ * 19 JUL 2016
+ */
+
+
+var DOMBuilder = function () {
+	this.TAGS_SELF_CLOSING = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr']
+	
+	this.body = '';
+	this.tags = [];
+};
+
+Builder.prototype.toString = function () {
+	return this.body;
+};
+
+Builder.prototype.parse = function (document) {
+	var wrapper = document.createElement('div');
+	wrapper.innerHTML = this.body;
+
+	return wrapper.firstChild;
+};
+
+Builder.prototype.ele = function (tag, arg2, arg3) {
+	this.body = this.body.concat('<'+tag);
+
+	if (typeof arg2 == 'object') {
+		for (var attr in arg2) {
+			this.body = this.body.concat(' '+attr+'='+'"'+arg2[attr]+'"');
+		}
+	}
+
+	if (this.TAGS_SELF_CLOSING.indexOf(tag) == -1) {
+		this.body = this.body.concat('>');
+
+		if (typeof arg2 == 'string') {
+			this.body = this.body.concat(arg2).concat('</'+tag+'>');
+		} else if (typeof arg3 == 'string') {
+			this.body = this.body.concat(arg3).concat('</'+tag+'>');
+		} else {
+			this.tags.push(tag);
+		}
+
+	} else {
+		this.body = this.body.concat('/>');
+	}
+	
+	return this;
+};
+
+Builder.prototype.val = function (text) {
+	this.body = this.body.concat(text);
+	return this;
+};
+
+Builder.prototype.cl = function () {
+	this.body = this.body.concat('</'+this.tags.pop()+'>');
+	return this;
+};
+
+if ( typeof module === "object" && typeof module.exports === "object" ) {
+	module.exports = DOMBuilder;
+}
